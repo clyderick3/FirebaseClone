177db934fc41545cba38df885ec2ffbda323ca7d
Clyde Broderick <clyderick3@gmail.com>
2022-04-30 18:23:21 -0400

    Created header file for all things header.

---
+++ mytube/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//Download es7 snippets as extension on VS Code for quick rfce shortcuts.
+
+function Header() {
+    return <div className = "header" >
+
+    </div>
+}
+
+export default Header;
