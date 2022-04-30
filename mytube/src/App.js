34a38328c170a020b477c09b57f8dac159e7a509
Clyde Broderick <clyderick3@gmail.com>
2022-04-30 18:20:18 -0400

    app js starting to create components.

--- mytube/src/App.js
+++ mytube/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import React from "react";
 
 function App() {
   return (
+      //class naming convention
     <div className="app">
       <h1>Hello there friends, let's build our YouTube Clone.</h1>
+
+        {/*Header */}
+        {/*Sidebar*/}
+        {/*RecommendedVideos*/}
     </div>
   );
 }
