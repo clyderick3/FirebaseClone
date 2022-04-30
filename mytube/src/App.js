0d90a4e266d2e6fff38bfc0080a679149feb7f3d
Clyde Broderick <clyderick3@gmail.com>
2022-04-30 18:46:35 -0400

    Added material ui buttons.

--- mytube/src/App.js
+++ mytube/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from "react";
+import Header from "./Header";
 
 function App() {
   return (
       //class naming convention
     <div className="app">
-      <h1>Hello there friends, let's build our YouTube Clone.</h1>
+      {/*<h1>Hello there friends, let's build our YouTube Clone.</h1>*/}
+
+        <Header/>
 
         {/*Header */}
         {/*Sidebar*/}
--- mytube/src/Header.js
+++ mytube/src/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 
 function Header() {
     return <div className = "header" >
-
+        <h1>I am a header.</h1>
     </div>
 }
 
