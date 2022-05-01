6d638b03ec8e09b876643d40e602c7b40183bca2
Clyde Broderick <clyderick3@gmail.com>
2022-05-01 12:48:10 -0400

    Added hover and color options to sidebar.

--- mytube/src/Sidebar.js
+++ mytube/src/Sidebar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import SidebarRow from './SidebarRow';
+import HomeSharpIcon from '@mui/icons-material/HomeSharp';
+import WhatshotSharpIcon from '@mui/icons-material/WhatshotSharp';
+import SubscriptionsSharpIcon from '@mui/icons-material/SubscriptionsSharp';
 import "./Sidebar.css";
+
 
 function Sidebar() {
     return (
         <div className = "sidebar">
-            <SidebarRow title = "Home"/>
-            <SidebarRow title = "Trending"/>
-            <SidebarRow title = "Subscription"/>
+            <SidebarRow Icon = {HomeSharpIcon} title = "Home"/>
+            <SidebarRow Icon = {WhatshotSharpIcon} title = "Trending"/>
+            <SidebarRow Icon = {SubscriptionsSharpIcon} title = "Subscription"/>
 
 
         </div>
--- mytube/src/SidebarRow.js
+++ mytube/src/SidebarRow.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './SidebarRow.css';
-import HomeSharpIcon from '@mui/icons-material/HomeSharp';
 
-function SidebarRow({title}) {
+function SidebarRow({Icon, title}) {
     return (
         <div className = "sidebarRow">
-            <HomeSharpIcon/>
-            <h2>{title}</h2>
+            <Icon className = "sidebarRow_icon"/>
+            <h2 className = "sidebarRow_title">{title}</h2>
         </div>
     )
 }
