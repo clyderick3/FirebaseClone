bc0363a325340c00d6243c08e8bc758b6227da66
Clyde Broderick <clyderick3@gmail.com>
2022-04-30 23:44:38 -0400

    Successfully added YouTube logo.

--- mytube/src/App.js
+++ mytube/src/App.js
@@ -7,6 +7,10 @@ function App() {
   return (
     <div className="App">
       {/*<h1>Hello there friends, let's build our YouTube Clone :) .</h1>*/}
+        <img
+            src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+            alt=""
+            />
 
         <Header />
 
--- mytube/src/Header.js
+++ mytube/src/Header.js
@@ -4,7 +4,7 @@ import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 function Header() {
     return (
         <div className = "header">
-            <h1>I am a header.</h1>
+            <h1>My FAKE YouTube header.</h1>
             <MenuSharpIcon />
         </div>
     );
