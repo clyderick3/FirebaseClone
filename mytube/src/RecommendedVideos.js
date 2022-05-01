038f3146ef4aede5437e24b4cafd7638c33560a6
Clyde Broderick <clyderick3@gmail.com>
2022-05-01 11:34:05 -0400

    Added recommended videos js.

---
+++ mytube/src/RecommendedVideos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RecommendedVideos() {
+    return (
+        <div>
+            <h2>This is where recommended videos will be.</h2>
+        </div>
+    )
+}
+
+export default RecommendedVideos
