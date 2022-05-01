16643810cc2b4f1d23e73d952014def3cf50f2d4
Clyde Broderick <clyderick3@gmail.com>
2022-04-30 21:22:00 -0400

    Added firebaseConfig in firebasejs.

---
+++ mytube/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDm0cPoiQBsFEN3AKYhrHBKsZBF1-xmP8c",
+    authDomain: "clone-bde06.firebaseapp.com",
+    projectId: "clone-bde06",
+    storageBucket: "clone-bde06.appspot.com",
+    messagingSenderId: "512368180456",
+    appId: "1:512368180456:web:9ff65c064906a619a05d5d",
+    measurementId: "G-3T8LEELBQ2"
+};
