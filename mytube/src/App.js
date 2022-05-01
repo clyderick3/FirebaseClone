c49026f1e9f2dd237b545ad48c1065f2c5771d68
Clyde Broderick <clyderick3@gmail.com>
2022-05-01 11:31:46 -0400

    Added side bar.

--- mytube/src/App.js
+++ mytube/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from "react";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 import "./App.css";
 
 function App() {
@@ -8,33 +9,10 @@ function App() {
     <div className="App">
       {/*<h1>Hello there friends, let's build our YouTube Clone :) .</h1>*/}
         <Header />
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-        <h1>Test.</h1>
-          <h1>Test.</h1>
-          <h1>Test.</h1>
-          <h1>Test.</h1>
-          <h1>Test.</h1>
-          <h1>Test.</h1>
-          <h1>Test.</h1>
-
-
-
-        {/*Header */}
-        {/* Sidebar */}
-        {/* Recommendations */}
+        <div className="app_page">
+            <Sidebar />
+            {/* Recommendations */}
+        </div>
     </div>
   );
 }
---
+++ mytube/src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <div>
+            <h2>Sidebar here.</h2>
+        </div>
+    )
+}
+
+export default Sidebar
