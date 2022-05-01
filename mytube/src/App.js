28ca5fcf7e31bfe48b2f777d861d696ab6997c99
Clyde Broderick <clyderick3@gmail.com>
2022-04-30 21:55:55 -0400

    Working header. Good code.

--- mytube/src/App.js
+++ mytube/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from "react";
+import Header from "./Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello there friends, let's build our YouTube Clone :) .</h1>
+
+        <Header />
 
         {/*Header */}
         {/* Sidebar */}
