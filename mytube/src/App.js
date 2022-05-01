6ddab6a010529f2aecbe13dcfd2cfa637b8c7499
Clyde Broderick <clyderick3@gmail.com>
2022-04-30 21:48:28 -0400

    Added Header_js. Working code.

--- mytube/src/App.js
+++ mytube/src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hello there friends, let's build our YouTube Clone :) .</h1>
+
+        {/*Header */}
+        {/* Sidebar */}
+        {/* Recommendations */}
     </div>
   );
 }
