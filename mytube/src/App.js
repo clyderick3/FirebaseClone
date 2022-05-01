3fbdbf5efb313da56bd10cfe6b97be4ab351e683
Clyde Broderick <clyderick3@gmail.com>
2022-04-30 23:25:46 -0400

    Added menu icon with only header.

--- mytube/src/App.js
+++ mytube/src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello there friends, let's build our YouTube Clone :) .</h1>
+      {/*<h1>Hello there friends, let's build our YouTube Clone :) .</h1>*/}
 
         <Header />
 
--- mytube/src/Header.js
+++ mytube/src/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 
 function Header() {
     return (
         <div className = "header">
             <h1>I am a header.</h1>
+            <MenuSharpIcon />
         </div>
     );
 }
