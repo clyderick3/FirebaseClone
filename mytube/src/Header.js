adffc96ca8aaf30e8b4c887bc62fdddc1f8140ff
Clyde Broderick <clyderick3@gmail.com>
2022-05-01 11:21:26 -0400

    Created sticky header front layer.

--- mytube/src/Header.js
+++ mytube/src/Header.js
@@ -14,14 +14,12 @@ function Header() {
         <div className = "header">
             <div className="header_left">
                 <MenuSharpIcon />
-
-            </div>
-
             <img
                 className = "headerLogo"
                 src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
                 alt=""
             />
+            </div>
 
             <div className="header_input">
                 <input placeholder = "Search" type = "text"/>
@@ -29,9 +27,9 @@ function Header() {
             </div>
 
             <div className="header_icons">
-                <VideoCallSharpIcon/>
-                <AppsSharpIcon/>
-                <CircleNotificationsSharpIcon/>
+                <VideoCallSharpIcon className = "header_icon"/>
+                <AppsSharpIcon className = "header _icon"/>
+                <CircleNotificationsSharpIcon className = "header_icon"/>
                 <Avatar
                     alt = "Clizzy"
                     img src = "https://media-exp1.licdn.com/dms/image/C4E03AQG16NeR7pYFiw/profile-displayphoto-shrink_800_800/0/1651343221678?e=1657152000&v=beta&t=0Bkl_FzmtdndDUvI9ESOlzw3xtf_1XtkE-zMEkYGRLE" />
