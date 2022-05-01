b0664c8277c62d29eb480b8c4a5b6411ce86e59d
Clyde Broderick <clyderick3@gmail.com>
2022-05-01 11:46:16 -0400

    Added SidebarRow and setup welcome.

--- mytube/src/Sidebar.js
+++ mytube/src/Sidebar.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import SidebarRow from './SidebarRow';
 import "./Sidebar.css";
 
 function Sidebar() {
     return (
         <div className = "sidebar">
             <h2>Sidebar here.</h2>
+
+            <SidebarRow title = "Home"/>
+            <SidebarRow title = "Trending"/>
+            <SidebarRow title = "Subscription"/>
+
+
         </div>
     )
 }
