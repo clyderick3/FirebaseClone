eed51a62af651764d228296c98efd362b64a4ac7
Clyde Broderick <clyderick3@gmail.com>
2022-05-01 21:44:18 -0400

    Started to breakdown the video construction.

--- mytube/src/VideoCard.js
+++ mytube/src/VideoCard.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Avatar} from "@mui/material";
 
 function VideoCard ({ image, title, channel, views, timestamp, channelImage}) {
     return (
         <div>
-            <h4>Video </h4>
+            <img src = {image} alt = ""/>
+            <div className = "videoCard_info">
+                <Avatar
+                    className = "videoCard_avatar"
+                    alt = {channel}
+                    src = {channelImage}
+                    />
+                <div className = "video_text">
+                    <h4>{title}</h4>
+                    <p>{channel}</p>
+                    <p>
+                        {views} • {timestamp}
+                    </p>
+                </div>
+            </div>
         </div>
 );
                     }
