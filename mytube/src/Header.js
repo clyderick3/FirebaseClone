8da79ac9e75089b2214c9cdd5ae4b60351c49e78
Clyde Broderick <clyderick3@gmail.com>
2022-05-10 11:15:16 -0400

    Added WeTube logo.

--- mytube/src/Header.js
+++ mytube/src/Header.js
@@ -16,7 +16,7 @@ function Header() {
                 <MenuSharpIcon />
             <img
                 className = "headerLogo"
-                src=""
+                src="mytube/Content/WeTube.png"
                 alt=""
             />
             </div>
