0b7984e2dd7eb299276c3deec666b95e3bb7c603
Clyde Broderick <clyderick3@gmail.com>
2022-05-10 11:15:00 -0400

    Removed src file.

--- mytube/src/Header.js
+++ mytube/src/Header.js
@@ -16,7 +16,7 @@ function Header() {
                 <MenuSharpIcon />
             <img
                 className = "headerLogo"
-                src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+                src=""
                 alt=""
             />
             </div>
