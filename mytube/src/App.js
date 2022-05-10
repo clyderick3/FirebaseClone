5fc53fd5ff7de5d3f40805c1e108b628e3c881c3
Clyde Broderick <clyderick3@gmail.com>
2022-05-10 17:09:25 -0400

    Imported BrowserRouter. About to test switch function.

--- mytube/src/App.js
+++ mytube/src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//import "./App.css";
 
 function App() {
     return (
